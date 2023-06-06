1b61c7b9a6189fc1941b1bc0c2917d579915c852
Marvellous Adetunji <adetunjimarvellous09@gmail.com>
2023-06-06 20:37:10 +0100

    maintenance

--- src/components/PreviewCollections.jsx
+++ src/components/PreviewCollections.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ShopItem from "./shopItem";
+import ShopItem from "./ShopItem";
 import { NavLink } from "react-router-dom";
 
 const PreviewCollections = ({ title, routeName, items, preview }) => {
